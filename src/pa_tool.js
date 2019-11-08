9b2d60bdfe77ab51e482d5393242da9dae191ab7
andrchoi <andrew.tt.choi@gmail.com>
2019-11-08 11:39:12 -0800

    added sha1 implementation

--- src/pa_tool.js
+++ src/pa_tool.js
@@ -1,4 +1,4 @@
-const sha1 = require('js-sha1')
+sha1 = require('./sha1')
 
 let input = process.argv.slice(2)
 input = input.join(' ')
@@ -7,4 +7,6 @@ if (input === "") {
     console.log('Error: no input detected. \nUsage: pa_tool.js your_input_here')
 } else {
     console.log('Your password is '+input)
+    let hash = sha1(input)
+    console.log(hash)
 }
