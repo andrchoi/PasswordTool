43df811db2077688a01c9c8f2e0c9292d7b16661
andrchoi <andrew.tt.choi@gmail.com>
2019-11-08 15:38:26 -0800

    fixed sha1 encryption use

--- src/pa_tool.js
+++ src/pa_tool.js
@@ -1,12 +1,42 @@
-sha1 = require('./sha1')
+'use strict'
 
-let input = process.argv.slice(2)
-input = input.join(' ')
+async function getHIBPResponse(input) {
+    let index = 5;
+    let prefix = input.substring(0, index);
+    let suffix = input.substring(index, input.length-1);
+    let url = 'https://api.pwnedpasswords.com/range/';
 
-if (input === "") {
-    console.log('Error: no input detected. \nUsage: pa_tool.js your_input_here')
-} else {
-    console.log('Your password is '+input)
-    let hash = sha1(input)
-    console.log(hash)
+    try {
+        let res = await fetch(url+prefix);
+        console.log(res);
+        isPwned(res, suffix);
+    } catch(err) {
+        console.log(err);
+    }
+
 }
+
+function isPwned(list, input) {
+    list.forEach(element => {
+        if (input === element) {
+            return true;
+        }
+    });
+    return false;
+}
+
+function analyzeInput(input){
+    // let input = process.argv.slice(2);
+    // input = input.join(' ');
+    
+    if (input === "") {
+        console.log('Error: no input detected. \nUsage: pa_tool.js your_input_here');
+    } else {
+        console.log('Your password is '+input);
+        let hash = SHA1(input)
+        console.log(hash)
+        console.log(getHIBPResponse(hash));
+    }
+}
+
+analyzeInput('test')
